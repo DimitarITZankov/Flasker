d0ec4f4a6b24a9cf7b376bf921c6df05ddbb87ea
Mitko <dimitarzankovit@gmail.com>
2025-10-08 11:15:53 +0300

    new stuffs

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash
+from flask_wtf import FlaskForm
+from wtforms import SubmitField,StringField
+from wtforms.validators import DataRequired #requires to put data,nullable=false
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+#Create a From Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?",validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def index():
@@ -18,3 +26,15 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+    return render_template('name.html',
+                           name=name,
+                           form=form,)
